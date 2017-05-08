41052d4fd8461887717ee1111469b9129f4dc2ad
stulacy <stuart.lacy@gmail.com>
2017-05-08 16:13:41 +0100

    Running simulations in parallel now works

--- desshiny/server/simulation.R
+++ desshiny/server/simulation.R
@@ -196,10 +196,11 @@ simoutput <- eventReactive(input$runmultiplesimbutton, {
 
     withProgress(message="Running simulations", value=0, max=n_sims, {
         #print(system.time({
-            if (platform == "unix" && n_sims > 1e6) {
+            if (platform == "unix" && n_sims > 1) {
                 end_states <- mclapply(seq(n_sims), function(i) {
                         incProgress(1, detail=paste(i))
-                        run_simulation_cpp()
+                        run_simulation_cpp(trans_mat, num_inds, entry_rate, censor_time,
+                                           reactiveValuesToList(attributes), reactiveValuesToList(transitions))
                     },
                     mc.cores=4)
             } else {
