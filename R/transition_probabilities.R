ae9ba459617aea6251996205bc41ec44b1458562
stuart <stuart.lacy@gmail.com>
2018-06-12 22:55:07 +0100

    Preparation for release

--- R/transition_probabilities.R
+++ R/transition_probabilities.R
@@ -93,6 +93,35 @@ calculate_transition_probabilities <- function(occupancy, start_times, end_times
 #' @param ci Whether to calculate confidence intervals. See \code{flexsurv::pmatrix.simfs} for details.
 #' @param ci_margin Confidence interval range to use if \code{ci} is set to \code{TRUE}.
 #' @return A data frame with estimates of transition probabilities.
+#' 
+#' @examples 
+#' 
+#' library(multistateutils)
+#' library(mstate)
+#' library(flexsurv)
+#' 
+#' # Convert data to long
+#' data(ebmt3)
+#' tmat <- trans.illdeath()
+#' long <- msprep(time=c(NA, 'prtime', 'rfstime'), 
+#'                status=c(NA, 'prstat', 'rfsstat'), 
+#'                data=ebmt3, 
+#'                trans=tmat, 
+#'                keep=c('age', 'dissub'))
+#'                
+#' # Fit parametric models
+#' models <- lapply(1:3, function(i) {
+#'     flexsurvreg(Surv(time, status) ~ age + dissub, data=long, dist='weibull')
+#' })
+#' 
+#' # New individual to estimate transition probabilities for
+#' newdata <- data.frame(age="20-40", dissub="AML")
+#' 
+#' # Estimate transition probabilties at 1 year
+#' predict_transitions(models, newdata, tmat, times=365)
+#' 
+#' # Estimate transition probabilties at 1 year given we know they're alive after 6 months
+#' predict_transitions(models, newdata, tmat, times=365, start_times = 365/2)
 #'
 #' @importFrom magrittr '%>%'
 #' @import data.table
