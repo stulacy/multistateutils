afd84d4b5327f06658d523343700c517a6c91496
stulacy <stuart.lacy@gmail.com>
2018-06-29 17:02:23 +0100

    Cohort simulation is running

--- R/cohort.R
+++ R/cohort.R
@@ -1,39 +1,54 @@
-# Runs a cohort discrete event simulation
-#
-# @param models Models in the form of a list of \code{flexsurvreg} objects.
-# @param newdata Data frame with individual attributes
-# @param trans_mat Transition matrix
-# @param start_times Entry times of individuals specified in \code{newdata}.
-#  Can either be a single time that everyone enters at, or have as many values
-#   as rows in \code{newdata}. If NULL then everyone enter at t=0
-# @return A data frame with state entry times for each individual.
-# @export
-# @importFrom Rcpp evalCpp
+#' Runs a cohort discrete event simulation
+#'
+#' @inheritParams predict_transitions
+#' @param start_time Entry times of individuals specified in \code{newdata}.
+#'  Can either be a single time that everyone enters at, or have as many values
+#'   as rows in \code{newdata}. Defaults to everyone starting at time 0.
+#' @param start_state The starting state of the individuals specified in \code{newdata}.
+#'  States can be represented by an integer (the row/column number of the state in \code{trans_mat}),
+#'  or as a string giving the name of the state in \code{trans_mat}.
+#'  Can either be a single value when everyone starts in the same state, or have as many values
+#'   as rows in \code{newdata}. Defaults to everyone starting in state 1.
+#' @param time_limit The maximum time to run the simulation for. If not provided then
+#'   the simulation runs until all the individuals have obtained a sink state.
+#' @return A data frame with state entry times for each individual.
+#' @export
 cohort_simulation <- function(models, newdata, trans_mat, start_time=0, start_state=1,
                               time_limit=NULL,
                               tcovs=NULL, M=1e3, ci=FALSE, ci_margin=0.95) {
 
     N <- nrow(newdata)
-    
-    if (length(start_time) == 1) 
+
+    if (length(start_time) == 1)
         start_time <- rep(start_time, N)
-    
-    if (length(start_state) == 1) 
+
+    if (length(start_state) == 1)
         start_state <- rep(start_state, N)
-    
+
     # Guards
-    if (length(start_time) != N) 
+    if (length(start_time) != N)
         stop("Error: start_time must have either as many values as rows in newdata, or 1.")
-    if (length(start_state) != N) 
+    if (length(start_state) != N)
         stop("Error: start_time must have either as many values as rows in newdata, or 1.")
     # check start times all positive
-    if (!all(start_time > 0)) 
-        stop("Error: must have positive start_time.")
+    if (!all(start_time >= 0))
+        stop("Error: must have start_time >= 0.")
+    if (!is.null(time_limit)) {
+        if (!is.numeric(time_limit))
+            stop("Error: time_limit must be a positive number.")
+        if (time_limit <= 0)
+            stop("Error: time_limit must be a positive number.")
+    }
+
     start_state <- sapply(start_state, validate_starting_state, trans_mat)
-        
-    occupancy <- state_occupancy(models, trans_mat, newdata, tcovs, start_time, 
+
+    occupancy <- state_occupancy(models, trans_mat, newdata, tcovs, start_time,
                                  start_state, ci, M)
-    
-    # TODO what post-processing is required for cohort? Want to apply time-limit for starters
-    occupancy
+
+    if (!is.null(time_limit))
+        occupancy <- occupancy[time <= time_limit]
+
+    # Separate covariates out
+    clean <- separate_covariates(occupancy, colnames(newdata))
+    clean
 }
